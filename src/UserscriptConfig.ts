4d5da4c4535dd75c5213744b84928c63bded9843
rrowe404 <140030215+rrowe404@users.noreply.github.com>
2024-11-24 15:33:44 -0800

    use a constructor so we can pass in the cwd

--- src/UserscriptConfig.ts
+++ src/UserscriptConfig.ts
@@ -1,30 +1,39 @@
+import { Configuration, EntryObject, webpack } from "webpack";
 import { UserscriptPlugin } from "./UserscriptPlugin";
 
 const path = require('path');
 
-const userscriptPlugin = new UserscriptPlugin(__dirname);
+class UserscriptConfig {
+    private plugin: UserscriptPlugin;
 
-const UserscriptConfig = {
-    entry: userscriptPlugin.entry,
-    module: {
-        rules: [
-            { test: /\.ts$/, use: ['ts-loader'] }
-        ]
-    },
-    optimization: {
-        minimize: false,
-        usedExports: true
-    },
-    output: {
-        clean: true,
-        filename: '[name].user.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        userscriptPlugin
-    ],
-    resolve: {
-        extensions: ['.ts', '.js']
+    constructor(cwd: string) {
+        this.plugin = new UserscriptPlugin(cwd);
+    }
+
+    getConfiguration(): Configuration {
+        return {
+            entry: this.plugin.entry,
+            module: {
+                rules: [
+                    { test: /\.ts$/, use: ['ts-loader'] }
+                ]
+            },
+            optimization: {
+                minimize: false,
+                usedExports: true
+            },
+            output: {
+                clean: true,
+                filename: '[name].user.js',
+                path: path.resolve(__dirname, 'dist')
+            },
+            plugins: [
+                this.plugin
+            ],
+            resolve: {
+                extensions: ['.ts', '.js']
+            }
+        }
     }
 }
 
